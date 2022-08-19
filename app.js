eb737475eb089f066355c0837ebb447a1cf1b48c
jinguang <chjg1231@gmail.com>
2022-08-19 20:29:39 +0900

    에러 메세지 조정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 
-app.use("/", router);
+app.use("/api", router);
 app.use(express.json());
 app.listen(port, () =>{
     console.log(port, "포트로 서버가 열렸어요...")
--- routes/user.router.js
+++ routes/user.router.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const UsersController = require("../controllers/users.controller");
 const usersController = new UsersController();
 
-router.get("/",)
+router.post("/signup", usersController);
+router.post("/checkemail", usersController);
+router.post("/login", usersController);
 
 
 module.exports = router;
