f487ee8e03c4cdb20192f74ef45bfe200c3bd507
jinguang <chjg1231@gmail.com>
2022-08-25 01:00:08 +0900

    오타 수정 이메일 정규식 수정

--- middlewares/singup.middleware.js
+++ middlewares/singup.middleware.js
@@ -5,8 +5,8 @@ module.exports = async (req, res, next) => {
   const schema = Joi.object().keys({
     // 계정@도메인.최상위도메인 형식의 데이터
     email: Joi.string()
-      .email(),// 이메일 형태의 @ . 들어간 문자열
-    //   .pattern(new RegExp("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")),
+      // .email(),// 이메일 형태의 @ . 들어간 문자열
+      .pattern(new RegExp(/\S+@\S+\.\S+/)),
     // // 한글, 영문, 특수문자 (- _ .) 포함한 2 ~ 12글자 닉네임
     nickname: Joi.string(),//.pattern(new RegExp("^[a-zA-Zㄱ-힣0-9-_.]{2,12}$")),
     //   최소 8 자- 최대 13자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자
