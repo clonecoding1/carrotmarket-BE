c8e7010e901753c149b2a7d9c3e328d3c84af594
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-24 14:36:50 +0900

    커밋

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const cors = require("cors");
 const { sequelize } = require("./models");
 
-//  sequelize.sync({ force: true });
+ // sequelize.sync({ force: true });
 
 app.use(express.json());
 app.use(cors());
